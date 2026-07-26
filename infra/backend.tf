ee18994aed8178723f24f08a2d6444e2b90e0f6c
Deepak Kumar <deepak.ab.kumar0408@gmail.com>
2026-07-26 19:07:20 +0000

    Added azurerm provider and backend configuration

---
+++ infra/backend.tf
@@ -0,0 +1,9 @@
+terraform {    
+    backend "azurerm" {
+        resource_group_name  = "rg-bytebrain-tfstate-dev"
+        storage_account_name = "dpkdev01"
+        container_name       = "tfstate"
+        key                  = "bytebrain.tfstate"
+        use_azuread_auth     = true
+    }
+}
