f616f9ac698394c8ec708bb008fd14612293d5d7
Deepak Kumar <deepak.ab.kumar0408@gmail.com>
2026-07-31 20:31:18 +0000

    Corrected variables

--- infra/modules/key-vault/variables.tf
+++ infra/modules/key-vault/variables.tf
@@ -10,15 +10,6 @@ variable "key_vault_name" {
   type = string
 }
 
-variable "tenant_id" {
-  type = string
-}
-
-variable "object_id" {
-  type        = string
-  description = "Object ID of the identity (user/SP) allowed to manage secrets"
-}
-
 variable "tags" {
   type    = map(string)
   default = {}
